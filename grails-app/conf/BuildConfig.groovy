82f00ea8358a3fac31048d06ef65af573e92f21e
GS <rumee.gsl>
2014-11-03 11:32:20 +0600

    Change in PlugIn path!

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -37,6 +37,7 @@ grails.project.dependency.resolution = {
     checksums true // Whether to verify checksums on resolve
     legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
 
+
     repositories {
         inherits true // Whether to inherit repository definitions from plugins
 
